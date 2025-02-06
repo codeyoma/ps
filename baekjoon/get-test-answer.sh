e9d7605177de2f6c3b11bec1812a07b678afff6a
Yoma <codeyoma@gmail.com>
2025-02-06 15:16:27 +0900

    ...

--- baekjoon/get-test-answer.sh
+++ baekjoon/get-test-answer.sh
@@ -4,7 +4,7 @@ url_number=$1
 is_test=$2
 url="https://www.acmicpc.net/problem/$url_number"
 
-html=$(curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" $url)
+html=$(curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" $url)
 
 # echo $html
 count=$(echo $html | grep -o 'sample-output-' | wc -l)
@@ -12,7 +12,6 @@ count=$(expr $count / 2)
 
 
 for i in $(seq 1 $count); do
-    echo ""
     result=$(echo $html | perl -0777 -ne "print \$1 if /<pre class=\"sampledata\" id=\"sample-input-$i\">(.*?)<\/pre>/s")
     if [ "$is_test" == "true" ]; then
         echo $result > $url_number/test-input-$i.txt
@@ -28,6 +27,3 @@ for i in $(seq 1 $count); do
         echo $result
     fi
 done
-
-echo ""
-echo "Done!"
