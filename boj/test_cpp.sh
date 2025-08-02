0109cf8561409b7ef45e9d01ac2fcbc8b09f7060
yoma <codeyoma@gmail.com>
2025-08-02 16:57:31 +0900

    ...

--- boj/test_cpp.sh
+++ boj/test_cpp.sh
@@ -4,12 +4,15 @@ dir=$1
 test_count=$2
 target_test_number=$3
 
+
 if ls $dir/*.cpp >/dev/null 2>&1; then \
   echo ""
   echo "[cpp]"
   g++ -std=c++20 -Wall -Wextra -Werror -o $dir/$dir.out $dir/*.cpp; \
   $dir/$dir.out $dir $test_count $target_test_number; \
   ./check.sh $dir $test_count $target_test_number; \
+else \
+  cp template.cpp $dir/$dir.cpp; \
 fi
 
 echo
--- boj/test_python.sh
+++ boj/test_python.sh
@@ -8,4 +8,6 @@ if ls $dir/*.py >/dev/null 2>&1; then \
   echo "[python]"
   python3 $dir/$dir.py $dir $test_count $target_test_number; \
   ./check.sh $dir $test_count $target_test_number; \
+else \
+  cp template.py $dir/$dir.py; \
 fi
